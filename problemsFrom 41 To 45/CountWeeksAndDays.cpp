c34da45c7f055a335767c39ac14aa4dac6925e06
Youssif Alshafey <youssifalshafey@gmail.com>
2025-02-26 23:57:27 +0200

    Go

--- problemsFrom 41 To 45/CountWeeksAndDays.cpp
+++ problemsFrom 41 To 45/CountWeeksAndDays.cpp
@@ -12,9 +12,35 @@ int readNumberOfDays(string msg)
     cin >> days;
     return days;
 }
-
+stWeeksAndDays checkDays(int day)
+{
+    stWeeksAndDays weeksAndDays;
+    
+    if (day == weeksAndDays.saterDay)
+    {
+        cout << "Welcome In SaterDay Mode";
+    }else if(day == weeksAndDays.sunDay){
+        cout<<"Welcome In SunDay Mode";
+    }
+    else if(day == weeksAndDays.Monday){
+        cout<<"Welcome In Monday Mode";
+    }
+    else if(day == weeksAndDays.TusDay){
+        cout<<"Welcome In TusDay Mode";
+    }
+    else if(day == weeksAndDays.wednesDay){
+        cout<<"Welcome In wednesDay Mode";
+    }
+    else if(day == weeksAndDays.ThursDay){
+        cout<<"Welcome In ThursDay Mode";
+    }
+    else if(day == weeksAndDays.Friday){
+        cout<<"Welcome In Friday Mode";
+    }
+    return weeksAndDays;
+}
 int main()
 {
-    int totalDays = readNumberOfDays("Enter Number Of Days : ");
-    cout << totalDays;
+    int totalDays = readNumberOfDays("Enter Number Of Days : \n1 ) SaterDay \n2 ) Sunday \n3 ) MonDay \n4 )TusDay\n5 ) WednesDay\n6 )ThursDay\n7 )Friday\n ");
+    checkDays(totalDays);
 }
